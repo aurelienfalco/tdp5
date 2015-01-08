d38fe486fc83caff78557f5d2bce56178b632d3c
afalco <afalco@enseirb-matmeca.fr>
2015-01-08 19:17:24 +0100

    div / 0

--- dgetf2.c
+++ dgetf2.c
@@ -7,9 +7,14 @@ void LAPACKE_dgetf2(const enum CBLAS_ORDER matrix_order, int m, int n, double* a
 {
 	assert(matrix_order == CblasColMajor);
 	(void)ipiv;
+	double epsilon = 0.0001;
 
 	for (int i = 0; i < MIN(m,n); i++) {
-		cblas_dscal(m-i-1,1/a[i * lda+ i],&a[i * lda + i+1],1);
+		if (a[i * lda+ i] > epsilon)
+			cblas_dscal(m-i-1,1/a[i * lda+ i],&a[i * lda + i+1],1);
+		else if (a[i * lda+ i] != 0)
+			for (int j = 0; j < m-i-1; j++)
+				a[i * lda + i+j] /= a[i * lda+ i];
 		cblas_dger(matrix_order,m-i-1,n-i-1,-1,&a[i*lda + i+1],1,&a[(i+1)*lda + i],lda,&a[(i+1)*lda + i+1],lda);
 	}
 }
--- dscal.c
+++ dscal.c
@@ -2,10 +2,10 @@
 
 void cblas_dscal(int N, double da, double *dx, int incx)
 {
-    int i;
-    for (i = 0; i < N; i++)
-    {
-	*dx = *dx * da;
-	dx += incx;
-    }
+	int i;
+	for (i = 0; i < N; i++)
+	{
+		*dx = *dx * da;
+		dx += incx;
+	}
 }
