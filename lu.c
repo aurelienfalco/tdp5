38d607c382b98e538e432b338ab22c605e2b8af7
Nicoeclair <nicoeclair@hotmail.fr>
2015-01-09 12:48:41 +0100

    improved code

--- lu.c
+++ lu.c
@@ -6,8 +6,9 @@
 #include <string.h>
 #include <assert.h>
 
-// TODO: assigner de vrai tags aux messages
 //--map-by ppr:1:node -by-node
+#define TAG_COL_NUM 1
+#define TAG_COL_DATA 2
 
 void serpentinSuivant(int *value, int *inc, const int max) {
 	if (*inc) {
@@ -27,7 +28,7 @@ void serpentinSuivant(int *value, int *inc, const int max) {
 
 void mpi_cblas_lu(const enum CBLAS_ORDER order, const int m, const int n, double* a, int lda, int block_size)
 {
-	MPI_Datatype type_column;
+	MPI_Datatype type_column, tmp_type_block, type_block;
 	int blocks_per_line = (m/block_size);
 	//int disps[nb_blocks];
 	//int counts[nb_blocks];
@@ -40,88 +41,73 @@ void mpi_cblas_lu(const enum CBLAS_ORDER order, const int m, const int n, double
 	
 	MPI_Type_vector(1, col_size, col_size, MPI_DOUBLE, &type_column);
 	MPI_Type_commit(&type_column);
-	/*MPI_Type_vector(block_size, block_size, side_size, MPI_DOUBLE, &block_matrix);
-	MPI_Type_create_resized(block_matrix, 0, sizeof(double), &type_matrix);
-	MPI_Type_commit(&type_matrix);
-	
-	for (int i = 0; i < side_size; i++) {
-		for (int j = 0; j < side_size; j++) {
-			printf("i=%d\n", i);
-			disps[i * side_size + j] = i * blocks_by_line * block_size + j * block_size;
-			counts [i * side_size + j] = 1;
-		}
-	}
-	*/
+	MPI_Type_vector(block_size, block_size, side_size, MPI_DOUBLE, &tmp_type_block);
+	MPI_Type_create_resized(tmp_type_block, 0, block_size * sizeof(double), &type_block);
+	MPI_Type_commit(&type_block);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	MPI_Comm_size(MPI_COMM_WORLD, &nb_procs);
 
 	// Count the number of columns owned
-		next_val = 0; inc = 1;
-		for (i = 0; i < blocks_per_line; i++) {
-			if (next_val == myrank)
-				nb_cols++;
-			serpentinSuivant(&next_val, &inc, nb_procs);
-		}
-		cols_ref = malloc(nb_cols * sizeof(double));
-		left_col = malloc(col_size * sizeof(double));
-		columns = malloc(nb_cols * sizeof(double*));
-		for (i = 0; i < nb_cols; i++) {
-			columns[i] = malloc(col_size * sizeof(double));
-		}
-
+	next_val = 0; inc = 1;
+	for (i = 0; i < blocks_per_line; i++) {
+		if (next_val == myrank)
+			nb_cols++;
+		serpentinSuivant(&next_val, &inc, nb_procs);
+	}
+	cols_ref = malloc(nb_cols * sizeof(double));
+	left_col = malloc(col_size * sizeof(double));
+	columns = malloc(nb_cols * sizeof(double*));
+	for (i = 0; i < nb_cols; i++) {
+		columns[i] = malloc(col_size * sizeof(double));
+	}
+	
 	// ** Séparer la matrice avec le serpentin et le type_column par le proc 0, les autres reçoivent les données envoyées
-		if (myrank == 0) {
-			next_val = 0; inc = 1;
-			j = 0;
-			for (i = 0; i < blocks_per_line; i++) {
-				if (next_val == myrank) {
-					memcpy(columns[j], &a[i * col_size], col_size * sizeof(double));
-					cols_ref[j++] = i;
-				}
-				else {
-					MPI_Send(&i, 1, MPI_INT, next_val, 99, MPI_COMM_WORLD);
-				MPI_Send(&a[i * col_size], 1, type_column, next_val, 99, MPI_COMM_WORLD);// send to the right processus
+	if (myrank == 0) {
+		next_val = 0; inc = 1;
+		j = 0;
+		for (i = 0; i < blocks_per_line; i++) {
+			if (next_val == myrank) {
+				memcpy(columns[j], &a[i * col_size], col_size * sizeof(double));
+				cols_ref[j++] = i;
+			}
+			else {
+				MPI_Send(&i, 1, MPI_INT, next_val, TAG_COL_NUM, MPI_COMM_WORLD);
+				MPI_Send(&a[i * col_size], 1, type_column, next_val, TAG_COL_DATA, MPI_COMM_WORLD);// send to the right processus
 			}
 			serpentinSuivant(&next_val, &inc, nb_procs);
 		}
 	}
 	else {
 		for (i = 0; i < nb_cols; i++) {
-			MPI_Recv(&cols_ref[i], 1, MPI_INT, 0, 99, MPI_COMM_WORLD, &status);
-			MPI_Recv(columns[i], 1, type_column, 0, 99, MPI_COMM_WORLD, &status);
+			MPI_Recv(&cols_ref[i], 1, MPI_INT, 0, TAG_COL_NUM, MPI_COMM_WORLD, &status);
+			MPI_Recv(columns[i], 1, type_column, 0, TAG_COL_DATA, MPI_COMM_WORLD, &status);
 		}
 	}
 	
+	// LU decomposition
 	next_val = 0; inc = 1; j = 0;
 	for (int i = 0; i < MIN(m,n); i+=block_size) {
 		int ib = MIN(MIN(m,n)-i,block_size);
 		if (myrank == next_val) {
-
 			LAPACKE_dgetf2(order, m-i, ib, columns[j]+i, m, NULL);
-			memcpy(left_col, columns[j], col_size * sizeof (double));
-			MPI_Bcast(left_col, 1, type_column, next_val, MPI_COMM_WORLD);
+			memcpy(left_col, columns[j]+i, col_size * sizeof (double));
+			MPI_Bcast(left_col, blocks_per_line - (i / block_size), type_block, next_val, MPI_COMM_WORLD);
 			j++;
 		}
 		else {
-			MPI_Bcast(left_col, 1, type_column, next_val, MPI_COMM_WORLD);
+			MPI_Bcast(left_col, blocks_per_line - (i / block_size), type_block, next_val, MPI_COMM_WORLD);
 		}
-
+		
 		for(int k = j; k < nb_cols; k++) {
-			cblas_dtrsm(order, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, ib, ib, 1, left_col+i, lda, columns[k]+i, lda );
-
-			cblas_dgemm(order, CblasNoTrans, CblasNoTrans, m-i-ib, ib, ib, -1, left_col + i + ib, lda, columns[k]+i, lda, 1,  columns[k] + i + ib  , lda);
+			cblas_dtrsm(order, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, ib, ib, 1, left_col, lda, columns[k]+i, lda );
+			cblas_dgemm(order, CblasNoTrans, CblasNoTrans, m-i-ib, ib, ib, -1, left_col + ib, lda, columns[k]+i, lda, 1,  columns[k] + i + ib  , lda);
 		}
 
 		serpentinSuivant(&next_val, &inc, nb_procs);
 	}
-	// -- Envoyer la première colonne pour entamer les autres calculs
-	//MPI_Bcast(buf, 1, type_column, 0, MPI_COMM_WORLD);
 	
-	// ++ Chaque processus fait un trsm sur le premier bloc de sa colonne (il faut attendre le bloc de la première colonne)
-	// ++ puis fait un gemm sur les autres (il faut aussi attendre le bloc de la première colonne)
-	// print_matrix(left_col, side_size, block_size, side_size);
-	
+	// Gather data on proc 0
 	if (myrank == 0) {
 		next_val = 0; inc = 1; j = 0;
 		for (i = 0; i < blocks_per_line; i++) {
@@ -134,7 +120,7 @@ void mpi_cblas_lu(const enum CBLAS_ORDER order, const int m, const int n, double
 			serpentinSuivant(&next_val, &inc, nb_procs);
 		}
 	}
-	else{
+	else {
 		for (i = 0; i < nb_cols; i++) {
 			MPI_Send(columns[i], 1, type_column, 0, 99, MPI_COMM_WORLD);
 		}
@@ -144,6 +130,5 @@ void mpi_cblas_lu(const enum CBLAS_ORDER order, const int m, const int n, double
 	free(left_col);
 	for (i = 0; i < nb_cols; i++) {
 		free(columns[i]);
-	}
-	
+	}	
 }
--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 int main(int argc, char const *argv[])
 {
 	int myrank, scal = 0;
-	int m = 6, n = 6, block_size = 3;
+	int m = 16, n = 16, block_size = 4;
 	
 	MPI_Init(NULL, NULL);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
@@ -29,45 +29,7 @@ int main(int argc, char const *argv[])
 		print_matrix(U,m,n,m);
 	}
 	// double *a = rand_matrix(m, n);
-	
-	// a[0] = 1.1;
-	// a[1] = 1.1;
-	// a[2] = 1.1;
-	// a[3] = 1.1;
-	// a[4] = 1.1;
-	// a[5] = 1.1;
-	// a[6] = 2.2;
-	// a[7] = 2.2;
-	// a[8] = 2.2;
-	// a[9] = 2.2;
-	// a[10] = 2.2;
-	// a[11] = 2.2;
-	// a[12] = 3.3;
-	// a[13] = 3.3;
-	// a[14] = 3.3;
-	// a[15] = 3.3;
-	// a[16] = 3.3;
-	// a[17] = 3.3;
-	// a[18] = 4.4;
-	// a[19] = 4.4;
-	// a[20] = 4.4;
-	// a[21] = 4.4;
-	// a[22] = 4.4;
-	// a[23] = 4.4;
-	// a[24] = 5.5;
-	// a[25] = 5.5;
-	// a[26] = 5.5;
-	// a[27] = 5.5;
-	// a[28] = 5.5;
-	// a[29] = 5.5;
-	// a[30] = 6.6;
-	// a[31] = 6.6;
-	// a[32] = 6.6;
-	// a[33] = 6.6;
-	// a[34] = 6.6;
-	// a[35] = 6.6;
-
-
+		
 	if (scal){
 		fprintf(stderr, "Version scalaire %d\n", m);
 		if (myrank == 0)
